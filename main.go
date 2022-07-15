54b573c26d9a59a461f8eaa2f64d85c9da888f2c
monoflo <16790838+monoflo@users.noreply.github.com>
2022-07-15 13:15:33 -0400

    feat: print out CSV data

--- main.go
+++ main.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+    "encoding/csv"
+    "flag"
+    "fmt"
+    "os"
+)
 
 func main() {
-    fmt.Println("Hello world!")
+    csvFilename := flag.String("csv", "problems.csv", "a csv file in the format of 'question,answer'")
+    flag.Parse()
+
+    file, err := os.Open(*csvFilename)
+    if err != nil {
+        exit(fmt.Sprintf("Failed to open the CSV file: %s\n", *csvFilename))
+    }
+
+    r := csv.NewReader(file)
+    lines, err := r.ReadAll()
+    if err != nil {
+        exit("Failed to parse the provided CSV file\n")
+    }
+
+    fmt.Println(lines)
+}
+
+func exit(msg string) {
+    fmt.Printf(msg)
+    os.Exit(1)
 }
